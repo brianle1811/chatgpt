f68376d10e13b49b2092065f07bbf4855e5d250b
Brian Le <lehobaoanh1811@gmail.com>
2023-11-02 11:44:55 +0700

    Create chatgpt_ocr.py

---
+++ chatgpt_ocr.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+import constants
+
+import pytesseract
+from PIL import Image
+
+image_path = 'test.png'
+image = Image.open(image_path)
+text = pytesseract.image_to_string(image)
+output_file = 'data/output.txt'
+with open(output_file, 'w', encoding='utf-8') as file:
+    file.write(text)
+print(f"Extracted text saved to {output_file}")
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+PERSIST = False
+
+query = None
+if len(sys.argv) > 1:
+  query = sys.argv[1]
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  loader = DirectoryLoader("data/")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+  llm=ChatOpenAI(model="gpt-3.5-turbo"),
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
+chat_history = []
+
+while True:
+  if not query:
+    query = "Create list of 10 questions based on the output.txt?"
+        #query = "what is 3N101 and how to check level of 3N101?"
+        #query = "When we need to start the flow 3NV101?"
+        #query = input("Prompt: ")
+    #query = input("Prompt: ")
+  if query in ['quit', 'q', 'exit']:
+    sys.exit()
+  result = chain({"question": query, "chat_history": chat_history})
+  print(result['answer'])
+
+  chat_history.append((query, result['answer']))
+  query = None
